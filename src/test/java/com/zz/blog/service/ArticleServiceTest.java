87b3b9dfd1d4a317688b1c34068bfbcde84d299a
kknd9819 <kknd9819@qq.com>
2020-12-26 00:10:47 +0800

    测试一下fetch

--- src/test/java/com/zz/blog/service/ArticleServiceTest.java
+++ src/test/java/com/zz/blog/service/ArticleServiceTest.java
@@ -1,11 +1,14 @@
 package com.zz.blog.service;
 
 import com.zz.blog.entity.Article;
+import com.zz.blog.mapper.ArticleMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.util.Assert;
+import static org.mockito.Mockito.*;
 
 import java.util.List;
 
@@ -17,6 +20,9 @@ public class ArticleServiceTest {
 
     @Autowired
     private ArticleService articleService;
+
+    @MockBean
+    ArticleMapper articleMapper;
 
     @Test
     public void findArticleList() {
@@ -34,5 +40,13 @@ public class ArticleServiceTest {
     public void findById() {
         Article article = articleService.findById(1);
         Assert.isNull(article, "不能为空");
+    }
+
+    @Test
+    public void testMock() {
+        List list = mock(List.class);
+        when(list.get(0)).thenReturn("Hello World");
+        System.out.println(list.get(0));
+        System.out.println(list.size());
     }
 }
